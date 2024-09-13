5a9f6f3c85cc5508a870a74190455642726ade8f
Alexis Huynh <huynhalexis1126@gmail.com>
2024-09-13 19:42:59 -0400

    Change name of site

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Alexis Huynh Portfolio';
+export const SITE_DESCRIPTION = "I'm an undergraduate student at MIT studying Mechanical Engineering. I'm interested in sustainable technology and is currently doing research on hydrogen powertrain systems.";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
